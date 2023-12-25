d5e0e4d46309eddbad770d602430385a8b359f21
gabhyun kim <kimgb415@gmail.com>
2023-12-25 14:08:39 +0900

    device number allocation and cdev initialization

--- scull/main.c
+++ scull/main.c
@@ -22,24 +22,105 @@
 
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
+#include "scull.h"
+
+
+int scull_major =   SCULL_MAJOR;
+int scull_minor =   0;
+int scull_nr_devs = SCULL_NR_DEVS;	/* number of bare scull devices */
+
+// S_IRUGO: parameter that is read only by the world
+// S_IRUGO | S_IWUSR: parameter can only be modified by the root user
+module_param(scull_major, int, S_IRUGO);
+module_param(scull_minor, int, S_IRUGO);
+module_param(scull_nr_devs, int, S_IRUGO);
 
 MODULE_AUTHOR("Gaby, Kim");
 MODULE_DESCRIPTION("scull device driver in Linux Device Driver with linux 6.7.0");
 MODULE_LICENSE("GPL v2");
 
+struct scull_dev *scull_devices;	/* allocated in scull_init_module */
+
+struct file_operations scull_fops = {
+	.owner =    THIS_MODULE,
+};
+
+/*
+ * The cleanup function is used to handle initialization failures as well.
+ * Thefore, it must be careful to work correctly even if some of the items
+ * have not been initialized
+ */
+void scull_cleanup_module(void)
+{
+	dev_t devno = MKDEV(scull_major, scull_minor);
+
+	/* Get rid of our char dev entries */
+	if (scull_devices) {
+		for (int i = 0; i < scull_nr_devs; i++) {
+			cdev_del(&scull_devices[i].cdev);
+		}
+		kfree(scull_devices);
+	}
+
+	/* cleanup_module is never called if registering failed */
+	unregister_chrdev_region(devno, scull_nr_devs);
+}
+
+static void scull_setup_cdev(struct scull_dev *dev, int index)
+{
+	int err, devno = MKDEV(scull_major, scull_minor + index);
+	cdev_init(&dev->cdev, &scull_fops);
+	dev->cdev.owner = THIS_MODULE;
+	dev->cdev.ops = &scull_fops;
+	err = cdev_add(&dev->cdev, devno, 1);
+	/* Fail gracefully if need be */
+	if (err)
+		printk(KERN_ALERT "Error %d addding scull%d", err, index);
+}
 
 static int __init scull_init_module(void)
 {
-	printk(KERN_INFO "scull module loaded");
+	int result;
+	dev_t dev = 0;
+
+	/*
+	* Get a range of minor numbers to work with, asking for a dynamic
+	* major unless directed otherwise at load time.
+	*/
+	if (scull_major) {
+		dev = MKDEV(scull_major, scull_minor);
+		result = register_chrdev_region(dev, scull_nr_devs, "scull");
+	} else {
+		result = alloc_chrdev_region(&dev, scull_minor, scull_nr_devs,
+				"scull");
+		scull_major = MAJOR(dev);
+	}
+	if (result < 0) {
+		printk(KERN_WARNING "scull: can't get major %d\n", scull_major);
+		return result;
+	}
+
+	/* 
+	 * allocate the devices -- we can't have them static, as the number
+	 * can be specified at load time
+	 */
+	scull_devices = kmalloc(scull_nr_devs * sizeof(struct scull_dev), GFP_KERNEL);
+	if (!scull_devices) {
+		result = -ENOMEM;
+		goto fail;  /* Make this more graceful */
+	}
+	memset(scull_devices, 0, scull_nr_devs * sizeof(struct scull_dev));
+
+	for (int i = 0; i < scull_nr_devs; ++i)
+		scull_setup_cdev(&scull_devices[i], i);
 
 	return 0;
-}
 
-// cleanup function can't be marked as __exit, if it's used else where other than module_exit
-static void __exit scull_cleanup_module(void)
-{
-	printk(KERN_INFO "scull module unloaded");
-};
+fail:
+	scull_cleanup_module();
+	return result;
+}
 
 module_init(scull_init_module);
 module_exit(scull_cleanup_module);

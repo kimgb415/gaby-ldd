7c84687405e75a36c1555a7ac816a112113d304a
gabhyun kim <kimgb415@gmail.com>
2023-12-25 17:56:13 +0900

    complete definition of scull_dev in scull.h

--- scull/scull.h
+++ scull/scull.h
@@ -66,8 +66,24 @@ extern int scull_major;
 extern int scull_nr_devs;
 
 /*
+ * Representation of scull quantum sets.
+ */
+struct scull_qset {
+	void **data;
+	struct scull_qset *next;
+};
+
+/*
  * Prototypes for shared functions
  */
-
 ssize_t scull_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos);
 ssize_t scull_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos);
+
+struct scull_dev {
+	struct scull_qset *data;  /* Pointer to first quantum set */
+	int quantum;              /* the current quantum size */
+	int qset;                 /* the current array size */
+	unsigned long size;       /* amount of data stored here */
+	struct semaphore sem;     /* mutual exclusion semaphore     */
+	struct cdev cdev;	  /* Char device structure		*/
+};

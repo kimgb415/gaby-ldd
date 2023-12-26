693a835f76376dc3e9c296e49fd037abe2b78958
gabhyun kim <kimgb415@gmail.com>
2023-12-26 17:59:24 +0900

    python script to read from and write to scull0

---
+++ scull/test_read_and_write.py
@@ -0,0 +1,40 @@
+import os
+import argparse
+
+scull_device = os.path.join(os.path.sep, "dev", "scull0")
+
+def write_to_device(device=scull_device, bytes=7000):
+    data_to_write = 'A' * bytes
+    try:
+        with open(device, "w") as file:
+            ret = file.write(data_to_write)
+            print(f"write returned with {ret}")
+    except Exception as e:
+        print(e)
+
+def read_from_device(device=scull_device, bytes=5000):
+    try:
+        with open(device, "r") as file:
+            ret = data = file.read(bytes)
+            print(data)
+            print(f"read returned with {ret}")
+    except Exception as e:
+        print(e)
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("option")
+
+    return parser
+
+
+if __name__ == "__main__":
+    args = get_parser().parse_args()
+
+    if args.option == "write":
+        write_to_device()
+    elif args.option == "read":
+        read_from_device()
+
+    
